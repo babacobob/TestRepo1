6803582058e74a7726b4be5112cc4002f088d932
babacobob@gmail.com <babacobob@gmail.com>
2018-06-26 10:37:00 +1000

    branch test commit

--- openGitUp.sh
+++ openGitUp.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+#Branch Test script
 echo "Setting access to Git via CNTLM service - Local proxy config modified"
 export http_proxy=http://localhost:3128/
 export https_proxy=http://localhost:3128/
 export ftp_proxy=http://localhost:3128/
+
 
