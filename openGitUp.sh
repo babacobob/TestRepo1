e983fb04d179f8c2ba2019e37383c1324262c0ff
babacobob@gmail.com <babacobob@gmail.com>
2018-06-26 11:08:57 +1000

    spelling error

--- openGitUp.sh
+++ openGitUp.sh
@@ -2,7 +2,7 @@
 #Branch Test script
 #Second modify of script
 echo "Setting access to Git via CNTLM service - Local proxy config modified"
-econd test of branching
+econ "test of branching"
 export http_proxy=http://localhost:3128/
 export https_proxy=http://localhost:3128/
 export ftp_proxy=http://localhost:3128/
